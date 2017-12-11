a18b62aedb4a8e0ddef204e26abfced4bbbd07e3
wordtinker <wordtinker@yandex.ru>
2017-12-11 21:22:56 +0300

    Add simple logger

--- LangTools/LangTools/Logger.cs
+++ LangTools/LangTools/Logger.cs
@@ -6,11 +6,16 @@ namespace LangTools
 {
     public class SimpleLogger : ILoggerFacade
     {
+#if DEBUG
+        private const string fileName = "DEBUG Log.txt";
+#else
         private const string fileName = "Log.txt";
+#endif
         private string path;
         public SimpleLogger(string folder)
         {
             path = Path.Combine(folder, fileName);
+            Log("Logging session started.", Category.Info, Priority.Medium);
         }
         public void Log(string message, Category category, Priority priority)
         {
@@ -21,7 +26,17 @@ namespace LangTools
                     category.ToString().ToUpperInvariant(),
                     message,
                     priority.ToString());
-            File.AppendAllText(path, messageToLog);
+            
+            if (category == Category.Debug)
+            {
+#if DEBUG
+                File.AppendAllText(path, messageToLog);
+#endif
+            }
+            else
+            {
+                File.AppendAllText(path, messageToLog);
+            }
         }
     }
 }
